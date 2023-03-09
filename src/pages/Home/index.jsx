49137dcfc5bc4e760a7885ea684fc6619f0b3b91
dprincecoder <dprincecoder@gmail.com>
2023-03-09 13:36:53 +0100

    did homepage nav

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import HomeNavbar from "../../components/HomeNavbar";
 import styles from "./home.module.scss";
 
 const Homepage = () => {
-  return <div className={styles.homepage}>Homepage</div>;
+  return (
+    <div className={styles.homepage}>
+      <HomeNavbar />
+    </div>
+  );
 };
 
 export default Homepage;
