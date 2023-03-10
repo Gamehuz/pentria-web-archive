a8b60b08c71684d404e6438c3e3dbf19c466d287
dprincecoder <dprincecoder@gmail.com>
2023-03-10 13:00:57 +0100

    update api

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -1,7 +1,7 @@
 import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 
 const link = new HttpLink({
-  uri: "https://pentria-apiv1-4w2b.ondigitalocean.app/graphql",
+  uri: "https://pentria-apiv1-4w2bw.ondigitalocean.app/graphql",
 });
 
 const appolloClient = new ApolloClient({
