73fec47d387e6692c05090185638f8ccad07b967
dprincecoder <dprincecoder@gmail.com>
2023-03-15 12:57:57 +0100

    fix

--- src/pages/LoginOrSignUp/index.jsx
+++ src/pages/LoginOrSignUp/index.jsx
@@ -1,19 +1,21 @@
+import { Link } from "react-router-dom";
 import Button from "../../components/Button";
 import styles from "./loginorsignup.module.scss";
 
 const LoginOrSignUp = () => {
-    return (
-        <div className={styles.loginOrSignUp}>
-            <article>
-                <h1>Pentria</h1>
-                <Button bg={styles.login} text={"LOGIN"}/>
-                <Button bg={styles.signup} text={"SIGNUP"}/>
-                <p>Don’t wanna sign up now? You can do it later.</p>
-                <Button bg={styles.continue} text={"CONTINUE"}/>
-            </article>
-
-        </div>
-    )
-}
+  return (
+    <div className={styles.loginOrSignUp}>
+      <article>
+        <Link to="/">
+          <h1>Pentria</h1>
+        </Link>
+        <Button bg={styles.login} text={"LOGIN"} />
+        <Button bg={styles.signup} text={"SIGNUP"} />
+        <p>Don’t wanna sign up now? You can do it later.</p>
+        <Button bg={styles.continue} text={"CONTINUE"} />
+      </article>
+    </div>
+  );
+};
 
 export default LoginOrSignUp;
