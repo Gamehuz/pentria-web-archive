47ae061588ee18de1f9d26fd2debc9663fe2e2d9
dprincecoder <dprincecoder@gmail.com>
2023-03-27 18:43:41 +0100

    fix

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -1,16 +1,21 @@
-import { ApolloClient, ApolloLink, HttpLink, InMemoryCache } from "@apollo/client";
+import {
+  ApolloClient,
+  ApolloLink,
+  HttpLink,
+  InMemoryCache,
+} from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 
 const authLink = setContext((_, { headers }) => {
   // get the authentication token from local storage if it exists
-  const token = localStorage.getItem('auth-token');
+  const token = localStorage.getItem("pentriaAccessToken");
   // return the headers to the context so httpLink can read them
   return {
     headers: {
       ...headers,
       authorization: token ? `Bearer ${token}` : "",
-    }
-  }
+    },
+  };
 });
 
 const params = {
@@ -18,11 +23,11 @@ const params = {
     authLink,
     new HttpLink({
       uri: "https://pentria-apiv1-4w2bw.ondigitalocean.app/graphql",
-    })
+    }),
   ]),
   cache: new InMemoryCache(),
-  connectToDevTools: true
-}
+  connectToDevTools: true,
+};
 
 const appolloClient = new ApolloClient(params);
 
