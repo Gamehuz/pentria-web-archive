3693ba5e8620d2127491bf14440b6ae9fc0eaab1
Great Adams <kingifean@gmail.com>
2023-03-30 23:09:56 +0100

    receipt page

--- src/pages/Receipt/index.jsx
+++ src/pages/Receipt/index.jsx
@@ -1,26 +1,80 @@
-import Button from "../../components/Button";
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useState } from "react";
+import {
+//   BrowserRouter as Router,
+//   Link,
+  useLocation
+} from "react-router-dom";
+import { useLazyQuery } from '@apollo/client';
+import QRCode from "react-qr-code";
+
+
+// eslint-disable-next-line no-unused-vars
+import {GET_RECEIPT} from "../../graphql/queries/bookings";
+// eslint-disable-next-line no-unused-vars
+import ALL_SPACES from '../../graphql/queries/spaces';
 import Nav from "../../components/Nav";
 import styles from "./receipt.module.scss"
 import barcode from "./assets/barcode.png";
 
+function useQuery() {
+
+    const { search } = useLocation();
+  
+    return React.useMemo(() => new URLSearchParams(search), [search]);
+  }
 const Receipt = () => {
+    // const [reference, setReference] = useState(null);
+    let query = useQuery();
+    
+    const [getReceipt, { loading, data, error }] = useLazyQuery(GET_RECEIPT);
+
+    const ref = query.get('reference')
+    // setReference(ref)
+
+        console.log(ref)
+    useEffect(() => {
+        getReceipt({ variables: { txRef: query.get('reference') } })
+    }, [])
+
+    if (error) {
+        return (
+            <div>
+                <Nav />
+                <div className={styles.error}>
+                    <h1>Oops!</h1>
+                    <p>Something went wrong. Please try again.</p>
+                </div>
+            </div>
+        )
+    }
+
     return (
         <div className={styles.receipt}>
             <Nav />
+            {!data? <div className={styles.loading}>Loading...</div> :
+
+            <>
             <div className={styles.download}>
-                <span>Pleasure Park</span>
-                <Button text={'DOWNLOAD'}/>
+                <span>{data.booking.spaceId.name}</span>
             </div>
             <section className={styles.details}>
                 <div>
                     <div className={styles.name}>
-                        <p>JOHN DOE</p>
-                        <span>081*****4567</span>
+                        <p>{ data.booking.customer.firstName } {data.booking.customer.lastName}</p>
+                        <span>{data.booking.customer.phone}</span>
                     </div>
                     <div className={styles.reservation}>
                         <p>Reservation At:</p>
-                        <span>PH Pleasure Park
-                        1234 Aba Rd, Port Harcourt, RI</span>
+                        <span>{`${data.booking.spaceId.name} ${data.booking.spaceId.location}`}</span>
+                    </div>
+                    <div className={styles.reservation}>
+                        <p>Booking status:</p>
+                        <span>{data.booking.status}</span>
+                    </div>
+                    <div className={styles.reservation}>
+                        <p>Payment status:</p>
+                        <span>{data.booking.payment}</span>
                     </div>
                 </div>
                 <div className={styles.eventDetails}>
@@ -32,33 +86,36 @@ const Receipt = () => {
                         <p>TIME</p>
                         <p>DURATION</p>
                     </div>
-                    <div>
-                        <p>1</p>
-                        <p>CHESS</p>
-                        <p>4 REGULAR</p>
-                        <p>13TH</p>
-                        <p>4 PM</p>
-                        <p>2 HRs</p>
+                   {data.booking.tickets.map((item, index) => {
+                    return (
+                        <div key={index}>
+                        <p>{index+1}</p>
+                        <p>{item.name}</p>
+                        <p>{item.count}</p>
+                        <p>{item.date}</p>
+                        <p>{item.time}</p>
+                        <p>{item.duration}</p>
                     </div>
-                    <div>
-                        <p>2</p>
-                        <p>ARCADE</p>
-                        <p>4 REGULAR</p>
-                        <p>13TH</p>
-                        <p>1 PM</p>
-                        <p>2 HRs</p>
-                    </div>
+                    )
+                   })}
                 </div>
-                <p className={styles.paid}>PAID: NGN 23,450</p>
+                <p className={styles.paid}>PAID: {data.booking.currency} {data.booking.total}</p>
                 <div className={styles.specialrequest}>
                     <p>Special Request:</p>
-                    <span>We plenty o, please provide enough space. Thanks</span>
+                    <span>{data.booking.specialRequest}</span>
                 </div>
                 <div className={styles.barcode}>
-                    <img src={barcode} alt="barcode"/>
-                    <span>P001234FY</span>
+                    <QRCode
+                        size={156}
+                        style={{ height: "auto", maxWidth: "50%", width: "50%" }}
+                        value={`Booking payment was ${data.booking.payment} and booking is still ${data.booking.status}`}
+                        viewBox={`0 0 156 156`}
+                    />
+                    <span>{query.get('reference')}</span>
                 </div>
-            </section>   
+            </section>  
+            </>
+            }
         </div>
     )
 }
