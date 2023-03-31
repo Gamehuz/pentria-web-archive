234cdd99bfab242407cac488e1f98542bb01fbda
dprincecoder <dprincecoder@gmail.com>
2023-03-31 05:20:28 +0100

    created withdrawal page

---
+++ src/components/ButtonSpiner/index.jsx
@@ -0,0 +1,10 @@
+// import React from "react";
+import styles from "./spinner.module.scss";
+
+const Spinner = () => (
+  <div className={styles.spinner}>
+    <div className={styles.spinner__ring}></div>
+  </div>
+);
+
+export default Spinner;
--- src/pages/UserDashboard/UserEarnings/index.jsx
+++ src/pages/UserDashboard/UserEarnings/index.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Button from "../../../components/Button";
 import styles from "./earnings.module.scss";
 
