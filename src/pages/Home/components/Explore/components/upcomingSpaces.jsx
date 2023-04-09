855e460f12310cd516759850e1413e6d4b69727d
dprincecoder <dprincecoder@gmail.com>
2023-04-09 10:20:18 +0100

    fixes

--- src/pages/Home/components/Explore/components/upcomingSpaces.jsx
+++ src/pages/Home/components/Explore/components/upcomingSpaces.jsx
@@ -1,68 +1,87 @@
-import { ReactComponent as Heart } from "../assets/heart.svg";
+import { useMutation } from "@apollo/client";
+import moment from "moment";
+import PropTypes from "prop-types";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Button from "../../../../../components/Button";
+import ADD_TO_FAVORITES from "../../../../../graphql/mutations/addToFavorites";
 import { ReactComponent as NoStar } from "../../../assets/no-star.svg";
 import { ReactComponent as Star } from "../../../assets/star.svg";
-import Button from '../../../../../components/Button';
+import { ReactComponent as Heart } from "../assets/heart.svg";
 import styles from "../explore.module.scss";
-import { useState } from "react";
-import moment from "moment"
-import { useMutation } from "@apollo/client";
-import ADD_TO_FAVORITES from "../../../../../graphql/mutations/addToFavorites";
-import PropTypes from "prop-types";
-import { useNavigate } from "react-router-dom";
 
 const UpcomingSpaces = ({ space }) => {
-    const [favorite, setFavorite] = useState(false);
-    const navigate = useNavigate()
+  const [favorite, setFavorite] = useState(false);
+  const navigate = useNavigate();
 
-    const toggleFavorite = () => {
-        setFavorite(!favorite)
-    };
-    
-    const spaceCreatedAt = new Date(Number(space.createdAt));
-    const spaceDate = moment(spaceCreatedAt).format("MMM DD, YYYY");
-    const spaceTime = moment(spaceCreatedAt).format("HH:mm");
+  const toggleFavorite = () => {
+    setFavorite(!favorite);
+  };
 
-    const [addToFavorites] = useMutation(ADD_TO_FAVORITES, {
-        variables: {
-            spaceId: space._id
-        }
-    });
+  const spaceCreatedAt = new Date(Number(space.createdAt));
+  const spaceDate = moment(spaceCreatedAt).format("MMM DD, YYYY");
+  const spaceTime = moment(spaceCreatedAt).format("HH:mm");
 
-    const spaceSencilo = (id) => {
-        navigate(`/sencilo/${id}`)
-    }
+  const [addToFavorites] = useMutation(ADD_TO_FAVORITES, {
+    variables: {
+      spaceId: space._id,
+    },
+  });
 
-    return (
-        <article key={space._id}>
-        <img src={space.image} alt="spaces arena"/>
-        <div className={styles.price}>
-            <span><p>{space.currency}{space.price}</p>/hr</span>
-            <div className={favorite ? styles.favorite : styles.unfavorite} onClick={() => {toggleFavorite(); addToFavorites()}}>
-                <Heart />
-            </div>
+  const spaceSencilo = (id) => {
+    navigate(`/sencilo/${id}`);
+  };
+
+  return (
+    <article key={space._id}>
+      <div className="flex w-full m-0 p-0">
+        <img src={space.image?.[0]} alt="spaces arena" className="w-full" />
+      </div>
+      <div className={styles.price}>
+        <span>
+          <p>
+            {space.currency}
+            {space.price}
+          </p>
+          /hr
+        </span>
+        <div
+          className={favorite ? styles.favorite : styles.unfavorite}
+          onClick={() => {
+            toggleFavorite();
+            addToFavorites();
+          }}
+        >
+          <Heart />
         </div>
-        <h3>{space.facilityType}</h3>
-        <p>{space.location}</p>
+      </div>
+      <h3>{space.facilityType}</h3>
+      <p>{space.location}</p>
+      <div>
         <div>
-            <div>
-                <Star />
-                <Star />
-                <Star />
-                <Star />
-                <NoStar />
-            </div>
-            <div className={styles.eventDate}>
-            <span>{spaceDate}</span>
-            <p>{spaceTime}</p>
-            </div>
-            <Button className={styles.button} type={'button'} text={'GET TICKET'} onClick={() => spaceSencilo(space._id)}/>
+          <Star />
+          <Star />
+          <Star />
+          <Star />
+          <NoStar />
         </div>
+        <div className={styles.eventDate}>
+          <span>{spaceDate}</span>
+          <p>{spaceTime}</p>
+        </div>
+        <Button
+          className={styles.button}
+          type={"button"}
+          text={"GET TICKET"}
+          onClick={() => spaceSencilo(space._id)}
+        />
+      </div>
     </article>
-    )
+  );
 };
 
 UpcomingSpaces.propTypes = {
-    space: PropTypes.object.isRequired
-}
+  space: PropTypes.object.isRequired,
+};
 
 export default UpcomingSpaces;
