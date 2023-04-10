c0a032cc977d02330e2e95741ee365407157d61c
dprincecoder <dprincecoder@gmail.com>
2023-04-10 23:18:47 +0100

    sales done for vendor

--- src/pages/VendorDashboard/Sales/compo/chart.jsx
+++ src/pages/VendorDashboard/Sales/compo/chart.jsx
@@ -2,21 +2,33 @@
 import { Line } from "react-chartjs-2";
 import {Chart as ChartJS} from 'chart.js/auto';
 
-const ChartGraph = ({ chartData}) => {
-
-  const chartOptions = {
-    scales: {
-      yAxes: [
-        {
-          ticks: {
-            beginAtZero: true,
-          },
-        },
-      ],
-    },
+const ChartGraph = ({ chartData }) => {
+  const labels = chartData.map((item) =>
+    new Date(item.created).toLocaleDateString()
+  );
+  const earnings = chartData.map((item) => item.amount);
+  const chartdata = {
+    labels,
+    datasets: [
+      {
+        label: "Sales",
+        data: earnings,
+        fill: false,
+        backgroundColor: [
+          "rgba(75,192,192,1)",
+          "#ecf0f1",
+          "#50AF95",
+          "#f3ba2f",
+          "#2a71d0",
+        ],
+        borderColor: "black",
+        borderWidth: 2,
+        tension: 0.1,
+      },
+    ],
   };
 
-  return <Line data={chartData} />;
+  return <Line data={chartdata}/>;
 };
 
 export default ChartGraph;
--- src/pages/VendorDashboard/Sales/index.jsx
+++ src/pages/VendorDashboard/Sales/index.jsx
@@ -11,56 +11,7 @@ import styles from "./earnings.module.scss";
 const VendorSales = () => {
   const firstTitle = ["Listings", "Date", "Amount"];
   const secondTitle = ["Customer", "Trx ID", "Status"];
-  const UserData = [
-    {
-      id: 1,
-      year: 2016,
-      userGain: 80000,
-      userLost: 823,
-    },
-    {
-      id: 2,
-      year: 2017,
-      userGain: 45677,
-      userLost: 345,
-    },
-    {
-      id: 3,
-      year: 2018,
-      userGain: 78888,
-      userLost: 555,
-    },
-    {
-      id: 4,
-      year: 2019,
-      userGain: 90000,
-      userLost: 4555,
-    },
-    {
-      id: 5,
-      year: 2020,
-      userGain: 4300,
-      userLost: 234,
-    },
-  ];
-  const [userData, setUserData] = useState({
-    labels: UserData.map((data) => data.year),
-    datasets: [
-      {
-        label: "Audience Reach",
-        data: UserData.map((data) => data.userGain),
-        backgroundColor: [
-          "rgba(75,192,192,1)",
-          "#ecf0f1",
-          "#50AF95",
-          "#f3ba2f",
-          "#2a71d0",
-        ],
-        borderColor: "black",
-        borderWidth: 2,
-      },
-    ],
-  });
+
   const { isLoading } = useSelector((state) => state.util);
   const [sales, setSales] = useState([]);
   const { user } = useSelector((state) => state.user);
@@ -103,7 +54,7 @@ const VendorSales = () => {
           />
         </div>
         <div className={styles.earningsGraph}>
-          <ChartGraph chartData={userData} />
+          <ChartGraph chartData={sales} />
         </div>
       </div>
     </div>
