e866df8ccb55dc3a5192118d6a5d275abc2e4a3d
dprincecoder <dprincecoder@gmail.com>
2023-05-01 06:02:27 +0100

    fixes

--- src/pages/TermOfUse/index.jsx
+++ src/pages/TermOfUse/index.jsx
@@ -1,16 +1,20 @@
 import Button from "../../components/Button";
+import HomeNavbar from "../../components/HomeNavbar";
 
 const TermOfUse = () => {
   return (
-    <div className="h-screen flex flex-col items-center justify-center">
-      <h1 className="text-4xl font-bold text-gray-800 mb-4">Coming Soon</h1>
-      <p className="text-gray-600 mb-8">
-        Our website is under construction. Stay tuned for updates.
-      </p>
-      <div className="flex space-x-4">
-        <Button text="Subscribe" to="/partners" />
+    <>
+      <HomeNavbar />
+      <div className="h-screen flex flex-col items-center justify-center">
+        <h1 className="text-4xl font-bold text-gray-800 mb-4">Coming Soon</h1>
+        <p className="text-gray-600 mb-8">
+          This page is under construction. Stay tuned for updates.
+        </p>
+        <div className="flex space-x-4">
+          <Button text="Subscribe" to="/partners" />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
